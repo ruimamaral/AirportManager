9ec7d1880e0156082ce87c967b70ccb0e99dbad5
someonearound <ruimiguel.amaral@hotmail.com>
2022-03-26 01:16:05 +0000

    setting up things pt2

--- projeto1.c
+++ projeto1.c
@@ -1,4 +1,32 @@
 # include <stdio.h>
 
-int main(){ # teste 1233
+int main(){
+	while (command_listener())
+}
+
+int command_listener() {
+	char command = getchar();
+	getchar() /* remove whitespace chararcter */
+	switch (command) {
+		case 'q':
+			return 0;
+		case 'a':
+			add_airport();
+			return 1;
+		case 'l':
+			list_airports();
+			return 1;
+		case 'v':
+			flights();
+			return 1;
+		case 'p':
+			departing();
+			return 1;
+		case 'c':
+			arriving();
+			return 1;
+		case 't':
+			set_time();
+			return 1;
+	}
 }
